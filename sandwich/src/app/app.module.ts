20c0e5f3824635a87d1a4960a973395ddd2a9ccf
zevyirmiyahu <zy@zevyirmiyahu.com>
2019-08-01 13:40:39 -0400

    Added login and navbar

--- sandwich/src/app/app.module.ts
+++ sandwich/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
